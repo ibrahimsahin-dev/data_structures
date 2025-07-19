abd5551cc8adfc2db2dda863873e2c3b01e2288c
ibrahim şahin <ibrahim.eray34@gmail.com>
2025-07-20 01:21:09 +0300

    ok

--- leetcode/21.cpp
+++ leetcode/21.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <vector>
 #include <stack>
+using namespace std;
   struct ListNode {
       int val;
       ListNode *next;
@@ -10,26 +11,66 @@
   };
 
  ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
-        ListNode* temp1=list1;
-        ListNode* temp2=list2;
-        ListNode* top;
-        while(temp2)
+    ListNode* temp1 = list1;
+    ListNode* temp2 = list2;
+
+    ListNode* top = new ListNode();
+    ListNode* adres = top;
+
+    while(temp1 && temp2)
+    {
+        if(temp1->val >= temp2->val)
         {
-            temp1->next=temp2;
-            temp1=temp1->next;
-            temp2=temp2->next;
+            ListNode* tempx = new ListNode(temp2->val);
+            cout << temp2->val << " deger temp2'den alindi\n";
+            adres->next = tempx;
+            adres = adres->next;
+            temp2 = temp2->next;
         }
-        top=temp1;
-      
-
-
+        else
+        {
+            ListNode* tempx = new ListNode(temp1->val);
+            cout << temp1->val << " deger temp1'den alindi\n";
+            adres->next = tempx;
+            adres = adres->next;
+            temp1 = temp1->next;
+        }
     }
 
+    while(temp1)
+    {
+        ListNode* tempx = new ListNode(temp1->val);
+        cout << temp1->val << " (kalan) temp1'den alindi\n";
+        adres->next = tempx;
+        adres = adres->next;
+        temp1 = temp1->next;
+    }
+
+    while(temp2)
+    {
+        ListNode* tempx = new ListNode(temp2->val);
+        cout << temp2->val << " (kalan) temp2'den alindi\n";
+        adres->next = tempx;
+        adres = adres->next;
+        temp2 = temp2->next;
+    }
+
+    return top->next;
+}
 
 
 
+int main() {
+  
+    ListNode* list1 = new ListNode(1, new ListNode(2, new ListNode(4)));
 
+    ListNode* list2 = new ListNode(1, new ListNode(3, new ListNode(4)));
 
-  int main(){
-
-  }
+    ListNode* merged = mergeTwoLists(list1, list2);
+   
+    while (merged) {
+        std::cout << merged->val << " ";
+        merged = merged->next;
+    }
+    return 0;
+}
---
+++ leetcode/stackquestion.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <vector>
+#include <stack>
+using namespace std;
+void compress(stack<int> &x){
+    
+}
+
+int main(){
+
+    stack<int> x;
+    x.push(1);
+    x.push(1);
+    x.push(2);
+    x.push(2);
+    x.push(3);
+
+}

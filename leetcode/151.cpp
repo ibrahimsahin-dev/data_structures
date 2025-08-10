aa5f0406e12f253a9852180097f2a8db67cba9b5
ibrahim şahin <ibrahim.eray34@gmail.com>
2025-08-10 18:04:53 +0300

    151

--- leetcode/151.cpp
+++ leetcode/151.cpp
@@ -5,12 +5,63 @@
 #include <string.h>
 using namespace std;
 
+// Input: s = "the sky is blue"
+// Output: "blue is sky the"
 string reverseWords(string s) {
+        string str="";
         int i=s.length()-1;
+        int sonterim=-1;
         while(i>=0)
         {
-            if(s[i])
+            if(i==0 && s[0]!=' ')
+            {
+                str+=s[0];
+                break;
+            }
+            if(s[i]==' ')
+            {
+                i--;
+                continue;
+            }
+            else{
+                if(sonterim==-1)
+                {
+                    sonterim=i;
+                }
+                if(i==1)
+                {
+                    if(s[i-1]==' ')
+                    {
+                    string st2=s.substr(1,sonterim+1);
+                    str+=st2;
+                    i=-1;
+                    }
+                    else{
+                    string st2=s.substr(0,sonterim+1);
+                    str+=st2;
+                    i=-1;
+                    }
+                    
+                }
+                else{
+                    if(s[i-1]==' ')
+                    {
+                        string st2=s.substr(i,sonterim-i+1);
+                        str+=st2;
+                        if(str[str.length()-1]!=' ')
+                            str+=' ';
+                        sonterim=-1;
+                    }
+                    i--;
+                }
+            }
         }
-
-
+        if(str[str.length()-1]==' ')
+            return str.substr(0,str.length()-1);
+        return str;
+    }
+    int main()
+    {
+        string x ="a good   example";
+        cout<<reverseWords(x);
     }

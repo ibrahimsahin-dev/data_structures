22e94f6371e4c37f75de6ab2b56fffd6e616352d
ibrahim şahin <ibrahim.eray34@gmail.com>
2025-08-18 23:50:47 +0300

    ad

--- leetcode/17.cpp
+++ leetcode/17.cpp
@@ -2,9 +2,45 @@
 #include <vector>
 #include <stack>
 using namespace std;
-vector<string> letterCombinations(string digits) {
-        int i,j,k;
-        i=j=k=0;
-        
-        
+vector<vector<int>> x;
+void rec(vector<int> dizi, int index,vector<int> elde){
+    int a=0;
+    for(int x:elde)
+    {
+        a+=x;
     }
+    if(a==7)
+    {
+        x.push_back(elde);
+    }
+    if(a>7)
+        return;
+    if(index==dizi.size())
+    {
+        return;
+    }
+    for(int i=index; i<dizi.size();i++)
+    {
+        elde.push_back(dizi[i]);
+        rec(dizi,i,elde);
+        elde.pop_back();
+    }
+ 
+}
+int main()
+{
+    vector<int> nums = {2, 3, 6, 7};
+    int target = 3;
+    bool used[3]{0};
+    vector<int> currentCombination;
+    rec(nums,0,currentCombination);
+    cout << "Toplamı " << target << " olan ikili kombinasyonlar:" << endl;
+    for (const auto& combo : x) {
+        cout << "[";
+        for (size_t i = 0; i < combo.size(); ++i) {
+            cout << combo[i] << (i == combo.size() - 1 ? "" : ", ");
+        }
+        cout << "]" << endl;
+    }
+
+}

66b990fd8df315c516954b7ea902686b0eedbaa3
ibrahim şahin <ibrahim.eray34@gmail.com>
2025-08-22 15:40:27 +0300

    187 1493 1004

--- leetcode/1004.cpp
+++ leetcode/1004.cpp
@@ -11,45 +11,14 @@ using namespace std;
 // Output: 10
 // Explanation: [0,0,1,1,1,1,1,1,1,1,1,1,0,0,0,1,1,1,1]
 int longestOnes(vector<int>& nums, int k) {
-        int net=0;
-        vector<int> dizi;
-        int kalan_hak=k;
-        int toplam=0;
-        for(int i=0; i<nums.size();i++)
-        {
-            if(net<=0)
-                net=0;
-            if(nums[i]==0)
-            {
-                if(kalan_hak>0)
-                {
-                    net++;
-                    kalan_hak-=1;
-                }
-                else{
-                    int temp_net=net;
-                    for(int j=i-net;j<=temp_net;j++)
-                    {
-                        if(nums[j]==0)
-                        {
-                            net--;
-                            break;
-                        }
-                        else{
-                            net--;
-                        }
-                    }
-                        
-                }
-            }
-            else
-                {net++;}
-        
-            if(net>toplam)
-                toplam=net;
-        }
-        
-        return toplam+1;
+       int sol=0;
+       int zero_count=0;
+       for(int i=0;i<nums.size();i++)
+       {
+        if(nums[i]==0)
+            zero_count++;
+    
+       }
        
     }
 int main()

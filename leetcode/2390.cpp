23f4a94342c6586dfbda6bb2c4f4ffea767e603c
ibrahim şahin <ibrahim.eray34@gmail.com>
2025-08-28 14:52:29 +0300

    ad

--- leetcode/2390.cpp
+++ leetcode/2390.cpp
@@ -1,20 +1,2 @@
-string str;
-       stack<char> sta;
-       for(int i=0;i<s.length();i++)
-       {
-           if(s[i]=='*')
-           {
-               sta.pop();
-               
-           }
-           else{
-           sta.push(s[i]);
-           }
-       }
-       while(!(sta.empty()))
-       {
-           str+=sta.top();
-           sta.pop();
-       }
-       reverse(str.begin(),str.end());
-       return str;    
+#include <bits/stdc++.h>
+using namespace std;

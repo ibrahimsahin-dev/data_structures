e2d864c599bfff8daaaa34263ee45f3215a23a0e
ibrahim şahin <ibrahim.eray34@gmail.com>
2025-08-30 13:55:59 +0300

    aa

--- leetcode/735.cpp
+++ leetcode/735.cpp
@@ -1,46 +1,66 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-vector<int> asteroidCollision(vector<int>& asteroids) {
+vector<int> asteroidCollision_bruteforce(vector<int>& asteroids) {
         vector<int> res;
         stack<int> stk;
-        bool is_enter=0;
+        
         stk.push(asteroids[0]);
-        int deneme=0;
+        
         for(int i =1; i<asteroids.size();i++)
         {
-        if(asteroids[i]<0 && stk.top()>0)
-        while(asteroids[i]<0 && stk.top()>0)
-        {
-            if(abs(asteroids[i])==abs(stk.top()))
+            if(stk.empty())
             {
-                is_enter=0;
-                stk.pop();
-                break;
+                stk.push(asteroids[i]);
+                continue;
             }
-            else if(abs(asteroids[i])<abs(stk.top()))
+            if(stk.top()>0 && asteroids[i]<0)
             {
-                is_enter=0;
-                break;
-            }
-            else{
-                is_enter=1;
-                stk.pop();
-                if(stk.empty())
+                if(abs(stk.top())>abs(asteroids[i]))
                 {
-                    stk.push(asteroids[i]);
-                    break;
+                    continue;
                 }
+                else if(abs(stk.top())==abs(asteroids[i]))
+                {
+                    stk.pop();
+                    continue;
+                }
+                else{
+                    stk.pop();
+                    if(stk.top()>0 && asteroids[i]<0)
+                {
+                    while(abs(stk.top())<abs(asteroids[i]))
+                    {
+                          
+                        if(abs(stk.top())>abs(asteroids[i]))
+                        {
+                            
+                            break;
+                        }
+                        else if(abs(stk.top())==abs(asteroids[i]))
+                        {
+                            stk.pop();
+                            break;
+                        }
+                        else{
+                            stk.top();
+                            if(stk.empty())
+                            {
+                                break;
+                            } 
+                        }
+                    }
+                }
+                else
+                     stk.push(asteroids[i]);
+                }
+                
             }
-            deneme=i;
+            else
+                stk.push(asteroids[i]);
+       
         }
-        else
-        {
-            stk.push(asteroids[i]);
-        }
-        }
-        if(is_enter)
-            stk.push(asteroids[deneme]);
+        
         while(!stk.empty())
         {
             res.push_back(stk.top());
@@ -50,3 +70,20 @@ vector<int> asteroidCollision(vector<int>& asteroids) {
         return res;
     }
 
+vector<int> asteroidCollision(vector<int>& asteroids)
+{
+    vector<int> res;
+    stack<int> stk;
+    for(int i: asteroids)
+    {
+        
+    }
+
+    while(!stk.empty())
+        {
+            res.push_back(stk.top());
+            stk.pop();
+        }
+        reverse(res.begin(),res.end());
+        return res;
+}

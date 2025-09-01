c36b9360ca3c9d962d33a5f93376932443b3d618
ibrahim şahin <104871489+ibrahimsahin-dev@users.noreply.github.com>
2025-09-01 23:44:52 +0300

    Update 1792.cpp

--- daily_leetcode/1792.cpp
+++ daily_leetcode/1792.cpp
@@ -1,43 +1,47 @@
 #include <bits/stdc++.h>
 using namespace std;
-double maxAverageRatio(vector<vector<int>>& classes, int extraStudents) {
-    unordered_map<int,int> mymap;
+   double maxAverageRatio(vector<vector<int>>& classes, int extraStudents) {
+    vector<double> vec;
         for(int i=0;i<classes.size();i++)
         {
-            double val=double(classes[i][0])/double(classes[i][1]);
-            mymap[i]=val;
+            double val=double(classes[i][1])-double(classes[i][0]);
+            vec.push_back(val);
         }
         int extra=extraStudents;
         while(extra>0)
         {
-            int x=mymap[0];
+            double x=vec[0]
             int index=0;
-            for(auto it:mymap)
+            for(int i=0;i<vec.size();i++)
             {
-                if(x>it.second)
+                if(x>vec[i])
                 {
-                    x=it.second;
-                    index=it.first;
+                    x=vec[i];
+                    index=i;
                 }
-                else if(x==it.second)
+                else if(x==vec[i])
                 {
-                    if(classes[index][1]<classes[it.first][1])
+                    if(classes[index][1]<classes[i][1])
+                    {;}
+                    else
                     {
-                        ;
+                    index=i;
                     }
-                    else{
-                        index=it.first;
-                    }
+                        
                 }
+                
             }
-            mymap[index]=double(classes[index][0]+1)/double(classes[index][1]+1);
+            classes[index][1]+=1;
+            classes[index][0]+=1;
+            vec[index]=double(classes[index][1])-double(classes[index][0])
             extra--;
         }
         double toplam=0;
-        for(auto it: mymap)
+        for(int i=0;i<classes.size();i++)
         {
-            toplam+=it.second;
+            double val=classes[i][1]/(classes[i][0]);
+            toplam+=val;
         }
-        toplam=toplam/double(mymap.size());
+        toplam=toplam/int(vec.size());
         return toplam;
     }

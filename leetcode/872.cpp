ac506463653bbf52ee8f8696b04b1062be31dd0f
ibrahim şahin <ibrahim.eray34@gmail.com>
2025-09-19 02:58:27 +0300

    a

--- leetcode/872.cpp
+++ leetcode/872.cpp
@@ -8,9 +8,36 @@ struct TreeNode {
     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
 };
-vector<int> arr1;
-vector<int> arr2;
-bool leafSimilar(TreeNode* root1, TreeNode* root2) {
-        
-    
+vector<int> numnode1;
+    vector<int> numnode2;
+    void rec(TreeNode* node,vector<int>& vec)
+{
+     if (node == nullptr) {
+        return;
+    }
+
+    if(node->left==nullptr && node->right==nullptr)
+    {
+        vec.push_back(node->val);
+        return;
+    }
+    rec(node->left,vec);
+    rec(node->right,vec);
+}
+    bool leafSimilar(TreeNode* root1, TreeNode* root2) {
+        rec(root1,numnode1);
+        rec(root2,numnode2);
+        cout<<" numnode1 elemanlari ";
+        for(int i: numnode1)
+        {
+            cout<<i;
+        }
+        cout<<endl<<endl;
+        cout<<"numnode2 elemanlari ";
+        for(int i: numnode2)
+        {
+            cout<<i;
+        }
+        return numnode1==numnode2;
+
     }
